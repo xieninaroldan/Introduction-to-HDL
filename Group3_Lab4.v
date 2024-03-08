c1afc399c66a453256080c7cf0ce6d6730ecd73f
Xienina Roldan <89092718+xieninaroldan@users.noreply.github.com>
2024-03-08 16:32:17 +0800

    Lab 4

---
+++ Group3_Lab4.v
@@ -0,0 +1,5 @@
+module Group3_Lab4(a1,a2,b,b1,b2);
+input a1,a2,b2;
+output b,b1;
+
+endmodule
---
+++ Group3_Lab4_full.v
@@ -0,0 +1,5 @@
+module Group3_Lab4_full(A,B,Bin,Diff,Bout);
+input A,B,Bin;
+output Diff,Bout;
+
+endmodule
